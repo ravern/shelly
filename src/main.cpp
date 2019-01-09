7a120cec482d27dd27f380eaba3cd3e7be703867
Ravern Koh <ravernkoh@gmail.com>
2019-01-09 11:44:42 +0800

    Create spell check word with dictionary

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,83 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 #include "Trie.h"
 
 using namespace std;
 
+// Loads all the words in the dictionary into the trie.
+bool loadDict(Trie *dict) {
+  ifstream dictionary;
+  dictionary.open("data/dictionary.txt");
+
+  if (!dictionary.is_open()) {
+    return false;
+  }
+
+  string word;
+  while (!dictionary.eof()) {
+    getline(dictionary, word);
+    dict->push(word);
+  }
+
+  dictionary.close();
+  return true;
+}
+
+// Displays the main menu.
+void printMainMenu() {
+  cout << "Welcome to Grammarly 2.0!" << endl;
+  cout << endl;
+  cout << " [1] Spell check a word" << endl;
+  cout << " [2] Spell check a file" << endl;
+  cout << " [0] Exit" << endl;
+  cout << endl;
+}
+
+// Spell checks a single word.
+void spellCheckWord(Trie *dict) {
+  cout << "Enter your word: ";
+
+  string word;
+  cin >> word;
+
+  if (dict->find(word)) {
+    cout << "Your word is spelled correctly!" << endl;
+  } else {
+    cout << "Your word is spelled wrongly!" << endl;
+  }
+
+  cout << endl;
+}
+
 int main() {
   Trie *dict = new Trie();
 
-  dict->push("john");
-  dict->push("mary");
+  // Load the dictionary into the trie.
+  if (!loadDict(dict)) {
+    cout << "Error: Failed to load dictionary!" << endl;
+    return 1;
+  }
 
-  cout << dict->find("john") << endl;
-  cout << dict->find("mary") << endl;
-  cout << dict->find("jane") << endl;
+  // Start the main loop.
+  while (true) {
+    printMainMenu();
+
+    int option;
+    cin >> option;
+    cout << endl;
+
+    switch (option) {
+    case 1:
+      spellCheckWord(dict);
+      break;
+    case 2:
+      break;
+    default:
+      return 0;
+    }
+  }
 
   return 0;
 }
